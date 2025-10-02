dd10712282fe0bc4e93769abe84fe5f9ce549d35
Nguyễn Hoàng Nam <hnamhocit@gmail.com>
2025-10-02 13:39:58 +0700

    init

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
-import express, {Request, Response} from "express";
+import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.get("/", (req: Request, res: Response) => {
-    res.send("🚀 Hello from Express + TypeScript!");
-});
 
 app.listen(PORT, () => {
     console.log(`✅ Server running at http://localhost:${PORT}`);
