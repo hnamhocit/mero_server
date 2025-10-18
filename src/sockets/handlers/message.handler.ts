ca504ee3780d246dd306501dde0ba5e4a8db49cb
Nguyễn Hoàng Nam <hnamhocit@gmail.com>
2025-10-19 00:17:06 +0700

    update

--- src/sockets/handlers/message.handler.ts
+++ src/sockets/handlers/message.handler.ts
@@ -10,6 +10,11 @@ export class MessageHandler extends BaseHandler {
     const messages = await this.prisma.message.findMany({
       where: {
         conversationId: conversationId,
+        deletions: {
+          none: {
+            userId: socket.user.id,
+          },
+        },
       },
       take: 20,
       skip: cursor ? 1 : 0,
@@ -22,6 +27,18 @@ export class MessageHandler extends BaseHandler {
         createdAt: "asc",
       },
       include: {
+        reply: {
+          select: {
+            id: true,
+            content: true,
+            sender: {
+              select: {
+                id: true,
+                displayName: true,
+              },
+            },
+          },
+        },
         sender: {
           select: {
             id: true,
@@ -36,12 +53,13 @@ export class MessageHandler extends BaseHandler {
   };
 
   send = async (socket: ISocket, args: any, cb: any) => {
-    const { content, conversationId } = args;
+    const { content, conversationId, replyId } = args;
 
     const newMessage = await this.prisma.message.create({
       data: {
         content,
         conversationId,
+        replyId,
         senderId: socket.user.id,
       },
       include: {
@@ -55,8 +73,66 @@ export class MessageHandler extends BaseHandler {
       },
     });
 
-    socket.to(`conversation-${conversationId}`).emit("message:new", newMessage);
+    this.io
+      .to(`conversation-${conversationId}`)
+      .emit("message:new", newMessage);
+  };
 
-    cb({ success: true, data: newMessage });
+  delete = async (socket: ISocket, args: any, cb: any) => {
+    const { id, conversationId } = args;
+    if (!id || !conversationId)
+      return cb({ success: false, message: "Message id is required" });
+
+    const messageCount = await this.prisma.message.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!messageCount)
+      return cb({ success: false, message: "Message not found" });
+
+    await this.prisma.message.delete({
+      where: {
+        id: id,
+      },
+    });
+
+    socket.to(`conversation-${conversationId}`).emit("message:deleted", id);
+  };
+
+  deleteForMe = async (socket: ISocket, args: any, cb: any) => {
+    const { id, conversationId } = args;
+    if (!id || !conversationId)
+      return cb({
+        success: false,
+        msg: "Id, conversationId are required",
+      });
+
+    const deletionCount = await this.prisma.messageDeletion.count({
+      where: {
+        messageId: id,
+        userId: socket.user.id,
+      },
+    });
+
+    if (deletionCount > 0) {
+      return cb({
+        success: false,
+        msg: "Message already deleted",
+      });
+    }
+
+    await this.prisma.messageDeletion.create({
+      data: {
+        messageId: id,
+        userId: socket.user.id,
+      },
+    });
+
+    return cb({
+      success: true,
+      msg: "Message deleted",
+    });
   };
 }
